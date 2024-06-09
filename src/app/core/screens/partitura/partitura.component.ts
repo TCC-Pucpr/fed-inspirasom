8c0e6571bdc02317d402f9cf88a8bed665c9e1ba
Kovalski <andre.kovalski@tecpuc.com.br>
2024-06-08 23:16:08 -0300

    integrando protótipo com mensageria via MIDI

--- src/app/core/screens/partitura/partitura.component.ts
+++ src/app/core/screens/partitura/partitura.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SecaoNotaComponent } from './secao-nota/secao-nota.component';
 import { CommonModule } from '@angular/common';
+import { RustDataSourceService } from '../../services/rust/dataSource/rust-dataSource.service';
+import { MidiSignal } from '../../model/MidiSignal';
+import { PartituraNotas } from '../../model/partituraNotas.model';
 
 @Component({
   selector: 'app-partitura',
@@ -12,6 +15,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './partitura.component.html',
   styleUrl: './partitura.component.scss'
 })
-export class PartituraComponent {
+export class PartituraComponent implements OnInit {
+
+  public notasIndex: number[] = [];
+
+  constructor(
+    private rustInvoker: RustDataSourceService
+  ){  }
+
+  ngOnInit(): void {
+    this.rustInvoker.listen_for_midi_note(this.andGetMidiNote);
+  }
+
+  public andGetMidiNote(signal: MidiSignal){
+    this.notasIndex.push(PartituraNotas.notas[signal.note])
+  }
 
 }
