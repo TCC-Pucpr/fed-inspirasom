f801cf10d17767885ba2874502205313f26b5429
Kovalski <andre.kovalski@tecpuc.com.br>
2024-06-08 23:36:30 -0300

    adicionando menu para modo gamificado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes, provideRouter } from "@angular/router";
-import { PartituraComponent } from "./core/screens/partitura/partitura.component";
 import { ApplicationConfig } from "@angular/core";
+import { PartituraComponent } from "./core/screens/menu-gamificado/telas/partitura/partitura.component";
+import { MenuGamificadoComponent } from "./core/screens/menu-gamificado/menu-gamificado.component";
 
 export const routes: Routes = [
-    
-    { path: '**', component: PartituraComponent }
+
+    { path: 'partitura', component: PartituraComponent },
+    { path: 'menu-gamificado', component: MenuGamificadoComponent },
+    { path: '**', component: MenuGamificadoComponent }
 ];
 
 export const routesConfig: ApplicationConfig = {
--- src/app/core/screens/menu-gamificado/telas/partitura/partitura.component.ts
+++ src/app/core/screens/menu-gamificado/telas/partitura/partitura.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SecaoNotaComponent } from './secao-nota/secao-nota.component';
 import { CommonModule } from '@angular/common';
-import { RustDataSourceService } from '../../services/rust/dataSource/rust-dataSource.service';
-import { MidiSignal } from '../../model/MidiSignal';
-import { PartituraNotas } from '../../model/partituraNotas.model';
+import { RustDataSourceService } from '../../../../services/rust/dataSource/rust-dataSource.service';
+import { MidiSignal } from '../../../../model/MidiSignal';
+import { PartituraNotas } from '../../../../model/partituraNotas.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-partitura',
@@ -20,11 +21,16 @@ export class PartituraComponent implements OnInit {
   public notasIndex: number[] = [];
 
   constructor(
-    private rustInvoker: RustDataSourceService
+    private rustInvoker: RustDataSourceService,
+    private router: Router
   ){  }
 
   ngOnInit(): void {
     this.rustInvoker.listen_for_midi_note(this.andGetMidiNote);
+  }
+
+  public voltaMenu() {
+    this.router.navigate(['menu-gamificado']);
   }
 
   public andGetMidiNote(signal: MidiSignal){
