09e12a2a2fa807a433874b07bd4ef3949dc75814
KnightLeo <knightleo602@gmail.com>
2024-06-09 00:56:03 -0300

    #5 mudando nome e outros ajustes

--- src-tauri/arduino-comm/src/midi_notes.rs
+++ src-tauri/arduino-comm/src/midi_notes.rs
@@ -10,27 +10,46 @@ pub enum Note {
 }
 
 impl Note {
+    pub const STATE_OFF: u8 = 128;
+    pub const STATE_ON: u8 = 144;
+    const MAX_VELOCITY: u8 = 127;
     pub fn ordinal(&self) -> u8 {
         *self as u8
     }
-    pub fn from_byte(byte: u8) -> Option<Note> {
+    pub fn from_byte(byte: u8) -> Option<Self> {
         Note::iter().get((byte - 55) as usize)
+    }
+    pub fn velocity_percentage(velocity: u8) -> u8 {
+        match velocity { 
+            0 => 0,
+            Self::MAX_VELOCITY => 100,
+            _ => velocity / Self::MAX_VELOCITY
+        }
     }
 }
 
 #[derive(Clone, Serialize, TS)]
 #[ts(export, export_to="../../../src/app/core/model/MidiSignal.ts", rename="MidiSignal")]
-pub struct NoteWrapper {
+pub struct MidiWrapper {
+    #[ts(rename="airFlow")]
     pub velocity: u8,
     pub note: Note
 }
 
-impl NoteWrapper {
-    pub fn new_from_bytes(byte: u8, velocity: u8) -> NoteWrapper {
-        NoteWrapper {
+impl MidiWrapper {
+    pub fn new_from_bytes(state: u8, byte: u8, velocity: u8) -> Self {
+        let note = if state == Note::STATE_ON {
+            *Note::from_byte(byte).get_or_insert(Note::A3)
+        } else {
+            Note::None
+        };
+        MidiWrapper {
             velocity,
-            note: *Note::from_byte(byte).get_or_insert(Note::A3)
+            note
         }
+    }
+    pub fn new_from_bytes_with_velocity_percentage(state: u8, byte: u8, velocity: u8) -> Self {
+        Self::new_from_bytes(state, byte, Note::velocity_percentage(velocity))
     }
 }
 
