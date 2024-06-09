eca961c898552e7a5e4d754af6f4a89a399cfb5b
Kovalski <andre.kovalski@tecpuc.com.br>
2024-06-08 23:23:59 -0300

    mudando configuração de routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
-import { Routes } from "@angular/router";
+import { Routes, provideRouter } from "@angular/router";
 import { PartituraComponent } from "./core/screens/partitura/partitura.component";
+import { ApplicationConfig } from "@angular/core";
 
 export const routes: Routes = [
+    
     { path: '**', component: PartituraComponent }
 ];
+
+export const routesConfig: ApplicationConfig = {
+    providers: [provideRouter(routes)],
+};
+  
