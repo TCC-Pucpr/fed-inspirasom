743ed02d7e247130b7abb8d45b94c349043068e0
Kovalski <andre.kovalski@tecpuc.com.br>
2024-06-10 19:25:15 -0300

    #15 correções para pegar a porta usb certa

--- src-tauri/arduino-comm/src/midi_connection.rs
+++ src-tauri/arduino-comm/src/midi_connection.rs
@@ -1,6 +1,8 @@
-use midir::{ConnectError, MidiInput, MidiInputConnection};
+use midir::{ConnectError, MidiInput, MidiInputConnection, MidiInputPort};
 
 use crate::midi_wrapper::MidiWrapper;
+
+const PORT_NAME: &str = "USB MidiKliK";
 
 pub fn connect<F: Fn(MidiWrapper) + Send + 'static>(callback: F) -> Result<MidiInputConnection<()>, ConnectError<MidiInput>> {
     let midi_in = MidiInput::new("MidiConnection").expect("Error");
@@ -10,27 +12,43 @@ pub fn connect<F: Fn(MidiWrapper) + Send + 'static>(callback: F) -> Result<MidiI
             panic!("Nao possui portas!")
         }
         1 => {
-            let name = midi_in.port_name(&ports[0]).unwrap();
-            println!("Uma unica porta encontrada: {}", name);
-            Some(ports[0].to_owned())
+            let port = ports[0].to_owned();
+            let port_name = midi_in.port_name(&port).unwrap();
+            if port_name.starts_with(PORT_NAME) {
+                Some(port)
+            } else {
+                None
+            }
         }
         _ => {
+            let mut port: Option<MidiInputPort> = None;
             println!("Varias portas encontradas!");
-            for p in ports.iter() {
-                println!("Porta: {}", midi_in.port_name(p).unwrap());
+            for p in ports {
+                let port_name = midi_in.port_name(&p).unwrap();
+                println!("Porta: {}", port_name);
+                if port_name.starts_with(PORT_NAME) {
+                    port = Some(p);
+                    break;
+                }
             }
-            Some(ports[0].to_owned())
+            port
         }
     };
-    midi_in.connect(
-        &input_port.unwrap(),
-        "Inspire", 
-        move |_, x: &[u8], _| {
-            println!("{:?} recebido", x);
-            if x.len() > 2 {
-                callback(MidiWrapper::new_from_bytes(x[0], x[1], x[2]))
-            }
-        },
-        ()
-    )
+    if let Some(p) = input_port {
+        let port_name = midi_in.port_name(&p).unwrap();
+        println!("Porta selecionada: {}", port_name);
+        midi_in.connect(
+            &p,
+            "Inspire", 
+            move |_, x: &[u8], _| {
+                println!("{:?} recebido", x);
+                if x.len() > 2 {
+                    callback(MidiWrapper::new_from_bytes(x[0], x[1], x[2]))
+                }
+            },
+            ()
+        )
+    } else {
+        panic!("Porta não encontrada!");
+    }
 }
