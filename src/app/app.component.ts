9e6e4e3013a0e7e7edb3dec13ef516ecfec08839
Kovalski <andre.kovalski@tecpuc.com.br>
2024-08-16 11:04:20 -0300

    #46 implementando serviço para troca de tema claro/escuro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule, DOCUMENT } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RustDataSourceService } from './core/services/rust/dataSource/rust-dataSource.service';
 
 import { ButtonModule } from 'primeng/button';
+import { ThemeService } from './core/services/theme-service/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -12,63 +13,16 @@ import { ButtonModule } from 'primeng/button';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
     constructor(
         private rustInvoker: RustDataSourceService,
-        @Inject(DOCUMENT) private document: Document
-    ) {
+        protected themeService: ThemeService
+    ) { }
+    
+    ngOnInit(): void {
+        this.themeService.initTheme();
     }
-
-    public themes = [
-        "primeng/resources/themes/bootstrap4-light-blue/theme.css",
-        "primeng/resources/themes/bootstrap4-light-purple/theme.css",
-        "primeng/resources/themes/bootstrap4-dark-blue/theme.css",
-        "primeng/resources/themes/bootstrap4-dark-purple/theme.css",
-        "primeng/resources/themes/md-light-indigo/theme.css",
-        "primeng/resources/themes/md-light-deeppurple/theme.css",
-        "primeng/resources/themes/md-dark-indigo/theme.css",
-        "primeng/resources/themes/md-dark-deeppurple/theme.css",
-        "primeng/resources/themes/mdc-light-indigo/theme.css",
-        "primeng/resources/themes/mdc-light-deeppurple/theme.css",
-        "primeng/resources/themes/mdc-dark-indigo/theme.css",
-        "primeng/resources/themes/mdc-dark-deeppurple/theme.css",
-        "primeng/resources/themes/fluent-light/theme.css",
-        "primeng/resources/themes/lara-light-blue/theme.css",
-        "primeng/resources/themes/lara-light-indigo/theme.css",
-        "primeng/resources/themes/lara-light-purple/theme.css",
-        "primeng/resources/themes/lara-light-teal/theme.css",
-        "primeng/resources/themes/lara-dark-blue/theme.css",
-        "primeng/resources/themes/lara-dark-indigo/theme.css",
-        "primeng/resources/themes/lara-dark-purple/theme.css",
-        "primeng/resources/themes/lara-dark-teal/theme.css",
-        "primeng/resources/themes/soho-light/theme.css",
-        "primeng/resources/themes/soho-dark/theme.css",
-        "primeng/resources/themes/viva-light/theme.css",
-        "primeng/resources/themes/viva-dark/theme.css",
-        "primeng/resources/themes/mira/theme.css",
-        "primeng/resources/themes/nano/theme.css",
-        "primeng/resources/themes/saga-blue/theme.css",
-        "primeng/resources/themes/saga-green/theme.css",
-        "primeng/resources/themes/saga-orange/theme.css",
-        "primeng/resources/themes/saga-purple/theme.css",
-        "primeng/resources/themes/vela-blue/theme.css",
-        "primeng/resources/themes/vela-green/theme.css",
-        "primeng/resources/themes/vela-orange/theme.css",
-        "primeng/resources/themes/vela-purple/theme.css",
-        "primeng/resources/themes/arya-blue/theme.css",
-        "primeng/resources/themes/arya-green/theme.css",
-        "primeng/resources/themes/arya-orange/theme.css",
-        "primeng/resources/themes/arya-purple/theme.css",
-        "primeng/resources/themes/nova/theme.css",
-        "primeng/resources/themes/nova-alt/theme.css",
-        "primeng/resources/themes/nova-accent/theme.css",
-        "primeng/resources/themes/luna-amber/theme.css",
-        "primeng/resources/themes/luna-blue/theme.css",
-        "primeng/resources/themes/luna-green/theme.css",
-        "primeng/resources/themes/luna-pink/theme.css",
-        "primeng/resources/themes/rhea/theme.css"
-    ]
 
     greetingMessage = "";
     isListeningMidi = false;
@@ -81,20 +35,6 @@ export class AppComponent {
             this.rustInvoker.connect_midi();
         }
         this.isListeningMidi = !this.isListeningMidi;
-    }
-
-    public changeTheme(newTheme: string) {
-        const themeLink = this.document.getElementById("app-theme") as HTMLLinkElement;
-        if(themeLink) {
-            themeLink.href = `${newTheme}.css`;
-        }
-    }
-
-    public changeThemeEasy(theme: number) {
-        const themeLink = this.document.getElementById("app-theme") as HTMLLinkElement;
-        if(themeLink) {
-            themeLink.href = `${theme}.css`;
-        }
     }
 
 }
