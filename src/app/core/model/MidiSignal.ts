481211ead59fb068adb05ee33ba3622ba9b66fa7
Kovalski <andre.kovalski@tecpuc.com.br>
2024-09-01 23:58:43 -0300

    #81 #108 adicionando areas para acerto da nota, correção do acerto de notas por ordem

--- src/app/core/model/MidiSignal.ts
+++ src/app/core/model/MidiSignal.ts
@@ -6,4 +6,6 @@
  * [`Self::note_index`] é o index da nota comecando no G3, ou seja, G3 é index 0 e
  * C5 é 15. Esse campo nunca vai ser negativo
  */
-export type MidiSignal = { note_index: number, is_bmol: boolean, note_name: string, airStrength: number, on: boolean, };
+
+//temporario, o nome da ultima variavel dps leo
+export type MidiSignal = { note_index: number, is_bmol: boolean, note_name: string, airStrength: number, state: boolean, };
