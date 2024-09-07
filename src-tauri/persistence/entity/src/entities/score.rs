61ce51b49de81254f45d8b6208cad0a735f5dd32
KnightLeo <knightleo602@gmail.com>
2024-09-07 18:33:51 -0300

    #60 id no score

--- src-tauri/persistence/entity/src/entities/score.rs
+++ src-tauri/persistence/entity/src/entities/score.rs
@@ -5,6 +5,8 @@ use sea_orm::entity::prelude::*;
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
 #[sea_orm(table_name = "score")]
 pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
     pub total: i32,
     pub date: DateTimeUtc,
     pub completed: bool,
