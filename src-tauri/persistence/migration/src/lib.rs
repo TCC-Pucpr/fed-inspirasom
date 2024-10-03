28b1787c0e544c7abdcf71cfa8210a1c5a084bab
KnightLeo <knightleo602@gmail.com>
2024-10-02 23:56:28 -0300

    #126 migration database

--- src-tauri/persistence/migration/src/lib.rs
+++ src-tauri/persistence/migration/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 mod music_json_loader;
 mod m20220101_000001_create_table;
 mod m20240908_003611_load_musics;
+mod m20241003_012943_extra_data;
 
 pub struct Migrator;
 
@@ -12,6 +13,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20240908_003611_load_musics::Migration),
+            Box::new(m20241003_012943_extra_data::Migration),
         ]
     }
 }
